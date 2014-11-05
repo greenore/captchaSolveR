5c9425bfee6682d146360be0a5a254fc551b4165
Tim Hagmann <greenore@users.noreply.github.com>
2014-11-05 23:18:03 +0100

    Update captcha.R

--- R/captcha.R
+++ R/captcha.R
@@ -353,27 +353,3 @@ rotateAndCombine <- function(letter){
   
   canvas
 }
-
-#' @title OCR with tesseract
-#' @export
-#' 
-#' @description \code{ocrTesseract} 
-#'  
-#' @param canvas
-#' 
-
-ocrTesseract <- function(canvas){
-  # Write canvas to tiff
-  writeTiff('prog_captcha/canvas.tiff', canvas)
-  
-  # OCR trough tesseract
-  system("tesseract C:/Users/CIB/Documents/R/web-scraping/prog_captcha/canvas.tiff C:/Users/CIB/Documents/R/web-scraping/prog_captcha/canvas letters", wait = T)
-  result <- paste(readLines("prog_captcha/canvas.txt"), collapse = "")
-  
-  # Gsub
-  result <- tolower(result)
-  result <- gsub(' ', '', result)
-  count <- sapply(letters, function(x){x <- sum(x == unlist(strsplit(result, "")))})
-  count
-  names(count)[count == max(count)]  
-}
